8810752cb5f88d5fecb49605d30eab7cea8a8fdb
Axel Cabral <axelcabral94@gmail.com>
2024-03-18 23:55:52 -0300

    Fin de preentrega 1

--- js/javascript.js
+++ js/javascript.js
@@ -9,10 +9,11 @@ let descripCod5 = 'Fiat 500X sport'
 let darBienvenida = '¡Bienvenidos a FerCars! \n\nPulse ENTER para ingresar'
 alert(darBienvenida)
 
-let seleccionProducto = parseInt(prompt('Seleccione el vehiculo que desea comprar \n1-Audi A3 sport back : 41000 USD \n2-Volkswagen sirocco GTS : 35000 USD \n3-Volkswagen golf GTI : 29000 USD \n4-Peugeot RCZ coupé : 24000 USD \n5-Fiat 500X sport : 25000 USD \n\nPulse 0 para finalizar compra'));
+let seleccionProducto = parseInt(prompt('Seleccione el vehiculo que desea comprar \n1-Audi A3 sport back : 41000 USD \n2-Volkswagen sirocco GTS : 35000 USD \n3-Volkswagen golf GTI : 29000 USD \n4-Peugeot RCZ coupé : 24000 USD \n5-Fiat 500X sport : 25000 USD \n\nPulse 0 para finalizar'));
 let total = 0;
+const IVA = 0.21;
 const DESCUENTO_TOTAL = 0.1;
-const TOTAL_FINANCIADO = 1.200;
+const INT_TOTAL_FINANCIADO = 0.15;
 
 while (seleccionProducto != 0) {
 
@@ -41,8 +42,34 @@ while (seleccionProducto != 0) {
             alert('Error en el codigo');
             break;
     }
-    seleccionProducto = parseInt(prompt('Seleccione el vehiculo que desea comprar \n1-Audi A3 sport back : 41,000 USD \n2-Volkswagen sirocco GTS : 35,000 USD \n3-Volkswagen golf GTI : 29,000 USD \n4-Peugeot RCZ coupé : 24,000 USD \n5-Fiat 500X sport : 25,000 USD \n\nPulse 0 para salir'));
+    seleccionProducto = parseInt(prompt('Seleccione el vehiculo que desea comprar \n1-Audi A3 sport back : 41,000 USD \n2-Volkswagen sirocco GTS : 35,000 USD \n3-Volkswagen golf GTI : 29,000 USD \n4-Peugeot RCZ coupé : 24,000 USD \n5-Fiat 500X sport : 25,000 USD \n\nPulse 0 para finalizar'));
 
 }
 
-let metodoPago = parseInt(prompt('Estas cerca de adquirir tu vehiculo.\n\n¿Con que metodo deseas abonarlo?\n\n1-Valor total en EFECTIVO con 10% de descuento en un pago\n2-Valor financiado con 200% de interes en 36 cuotas fijas'));
+if (total != 0) {
+    let metodoPago = parseInt(prompt('Estas cerca de adquirir tu vehiculo.\n\n¿Con que metodo deseas abonarlo?\n\n1-Valor total en EFECTIVO con 10% de descuento en un pago\n2-Valor financiado con 15% de interes en 36 cuotas fijas'));
+
+
+    if (metodoPago == 1) {
+        let totalPagar = aplicarImpuestoyDescuento(IVA, DESCUENTO_TOTAL, 0).toFixed(3);
+        alert('El total que debe abonar en efectivo es de: ' + totalPagar + 'UDS con impuestos incluhidos');
+    } else if (metodoPago == 2) {
+        let totalPagar = aplicarImpuestoyDescuento(IVA, 0);
+        let totalPagarInt = (totalPagar * (1 + INT_TOTAL_FINANCIADO)).toFixed(3);
+        let cuotas = (totalPagarInt / 36).toFixed(3);
+        alert('El total que debe abonar es de: ' + totalPagarInt + 'USD este valor ya tiene impluesto incluhido y su respectivo interes\n\nSu metodo de pago es de 36 cuotas fijas de:' + cuotas + 'USD');
+    } else {
+        alert('Opcion de pago invalida...');
+    }
+}
+
+function aplicarImpuestoyDescuento(impuesto, descuento) {
+    let totalConImpuesto = total * (1 + impuesto);
+    let totalConImpuestoyDescuento;
+    if (descuento != 0) {
+        totalConImpuestoyDescuento = totalConImpuesto * (1 - descuento);
+    } else {
+        totalConImpuestoyDescuento = totalConImpuesto;
+    }
+    return totalConImpuestoyDescuento;
+}
